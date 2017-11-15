302803c5d84b13ecf79edf754a5af0d355f83c5d
Salvador P <salvadorperezd@gmail.com>
2017-11-15 12:48:28 +0100

    Basic Redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
+import { addRecipe } from '../actions';
 
 class App extends Component {
+
+  state = {
+    calendar: null
+  }
+
+  componentDidMount() {
+    const {store} = this.props;
+    // Anytime the store is updated we update our state (calendar).
+    // There is a library react-redux which makes this faster and simpler...
+    store.subscribe(() => {
+        this.setState(() => ({
+          calendar: store.getState()
+        }))
+      })
+  }
+
+  /**
+   * When clicking on submit we call the action addRecipe adding the thing typed.
+   */ 
+  submitFood = () => {
+    this.props.store.dispatch(addRecipe({
+      day: 'monday',
+      meal: 'breakfast',
+      recipe: {
+        label: this.input.value
+      },
+    }));
+    this.input.value = '';
+  }  
+
   render() {
+    // Using the attribute ref allows us to modify directly the DOM without re-render.
+    // For more info check: https://reactjs.org/docs/refs-and-the-dom.html
     return (
       <div className="App">
-        Hello World
+        <input
+          type='text'
+          ref={(input) => this.input = input}
+          placeholder="Monday's Breakfast"
+        />
+        <button onClick={this.submitFood}>Submit</button>
+
+        <pre>
+          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
+        </pre>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import reducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+console.log(store.getState);
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
 registerServiceWorker();
+
+// Adding this line to the create store launches the debug tools at the browser.
+//  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
